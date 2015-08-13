89eb15647fb399d196c51c208e49c305ec95c9cf
Commander Hubot <john.paul.quicksall@gmail.com>
2015-08-13 14:27:38 -0700

    Added run file for local hubot to private repo

---
+++ runCommander.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+echo "Loading the commander!"
+HUBOT_SLACK_TOKEN=xoxb-8818119319-Nh48hS0f0PnC4GEWBiPpiFlt HUBOT_GOOGLE_CSE_KEY=AIzaSyDAvPLhJhrQRpENFaJQBkc0vPdyM8VmEKI ./bin/hubot -a slack
